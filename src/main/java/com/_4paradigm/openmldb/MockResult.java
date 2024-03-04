e39bbf15d8b726898708825663794f36c59904c5
yangwucheng <yangwucheng@4paradigm.com>
2024-03-04 15:25:44 +0800

    feat: mock result for sequel ace show tables

--- src/main/java/com/_4paradigm/openmldb/MockResult.java
+++ src/main/java/com/_4paradigm/openmldb/MockResult.java
@@ -53,6 +53,7 @@ public class MockResult {
     row = new ArrayList<>();
     row.add("Ssl_cipher");
     row.add("TLS_AES_256_GCM_SHA384");
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
     query = "show session variables like 'sql_mode'";
@@ -64,6 +65,7 @@ public class MockResult {
     row.add("sql_mode");
     row.add(
         "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION");
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
     query = "show session variables like 'version_comment'";
@@ -74,6 +76,7 @@ public class MockResult {
     row = new ArrayList<>();
     row.add("version_comment");
     row.add("");
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
     query = "show session variables like 'version'";
@@ -84,6 +87,7 @@ public class MockResult {
     row = new ArrayList<>();
     row.add("version");
     row.add(MySqlListener.VERSION);
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
     query = "show plugins";
@@ -129,6 +133,7 @@ public class MockResult {
     row = new ArrayList<>();
     row.add("skip_show_database");
     row.add("OFF");
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
     query = "show variables like 'character_set_database'";
@@ -139,9 +144,171 @@ public class MockResult {
     row = new ArrayList<>();
     row.add("character_set_database");
     row.add("utf8mb4");
+    rows.add(row);
     mockResults.put(query, new Pair<>(columns, rows));
 
-    String pattern = "(?i)SELECT .+ FROM .*information_schema.*\\..*routines.* WHERE .*routine_schema.* =.*";
+    query = "show tables in information_schema like 'engines'";
+    // # Tables_in_information_schema (ENGINES)
+    // ENGINES
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Tables_in_information_schema (ENGINES)", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("ENGINES");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query =
+        "select engine, support from `information_schema`.`engines` where support in ('default', 'yes') and engine != 'performance_schema'";
+    // # Engine, Support
+    // ARCHIVE, YES
+    // BLACKHOLE, YES
+    // MRG_MYISAM, YES
+    // MyISAM, YES
+    // InnoDB, DEFAULT
+    // MEMORY, YES
+    // CSV, YES
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Engine", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Support", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+
+    //    row = new ArrayList<>();
+    //    row.add("ARCHIVE");
+    //    row.add("YES");
+    //    rows.add(row);
+
+    //    row = new ArrayList<>();
+    //    row.add("BLACKHOLE");
+    //    row.add("YES");
+    //    rows.add(row);
+
+    //    row = new ArrayList<>();
+    //    row.add("MRG_MYISAM");
+    //    row.add("YES");
+    //    rows.add(row);
+
+    //    row = new ArrayList<>();
+    //    row.add("MyISAM");
+    //    row.add("YES");
+    //    rows.add(row);
+
+    row = new ArrayList<>();
+    row.add("InnoDB");
+    row.add("DEFAULT");
+    rows.add(row);
+
+    //    row = new ArrayList<>();
+    //    row.add("MEMORY");
+    //    row.add("YES");
+    //    rows.add(row);
+
+    //    row = new ArrayList<>();
+    //    row.add("CSV");
+    //    row.add("YES");
+    //    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query = "show variables like 'default_storage_engine'";
+    // # Variable_name, Value
+    // default_storage_engine, InnoDB
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Variable_name", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Value", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("default_storage_engine");
+    row.add("InnoDB");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query = "show variables like 'collation_database'";
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("Variable_name", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("Value", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("collation_database");
+    row.add("utf8mb4_0900_ai_ci");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query = "select * from `information_schema`.`character_sets` order by `character_set_name` asc";
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("CHARACTER_SET_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("DEFAULT_COLLATE_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("DESCRIPTION", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("MAXLEN", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    // # CHARACTER_SET_NAME, DEFAULT_COLLATE_NAME, DESCRIPTION, MAXLEN
+    // latin1, latin1_swedish_ci, cp1252 West European, 1
+    row = new ArrayList<>();
+    row.add("latin1");
+    row.add("latin1_swedish_ci");
+    row.add("cp1252 West European");
+    row.add("1");
+    rows.add(row);
+    // # CHARACTER_SET_NAME, DEFAULT_COLLATE_NAME, DESCRIPTION, MAXLEN
+    // utf8mb4, utf8mb4_0900_ai_ci, UTF-8 Unicode, 4
+    row = new ArrayList<>();
+    row.add("utf8mb4");
+    row.add("utf8mb4_0900_ai_ci");
+    row.add("UTF-8 Unicode");
+    row.add("4");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    query =
+        "select * from `information_schema`.`collations` where character_set_name = 'latin1' order by `collation_name` asc";
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("COLLATION_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("CHARACTER_SET_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("ID", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("IS_DEFAULT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("IS_COMPILED", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("SORTLEN", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PAD_ATTRIBUTE", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    // # COLLATION_NAME, CHARACTER_SET_NAME, ID, IS_DEFAULT, IS_COMPILED, SORTLEN, PAD_ATTRIBUTE
+    // latin1_swedish_ci, latin1, 8, Yes, Yes, 1, PAD SPACE
+    row = new ArrayList<>();
+    row.add("latin1_swedish_ci");
+    row.add("latin1");
+    row.add("8");
+    row.add("Yes");
+    row.add("Yes");
+    row.add("1");
+    row.add("PAD SPACE");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    // COLLATION_NAME, CHARACTER_SET_NAME, ID, IS_DEFAULT, IS_COMPILED, SORTLEN, PAD_ATTRIBUTE
+    query =
+        "select * from `information_schema`.`collations` where character_set_name = 'utf8mb4' order by `collation_name` asc";
+    // # COLLATION_NAME, CHARACTER_SET_NAME, ID, IS_DEFAULT, IS_COMPILED, SORTLEN, PAD_ATTRIBUTE
+    // utf8mb4_0900_ai_ci, utf8mb4, 255, Yes, Yes, 0, NO PAD
+    columns = new ArrayList<>();
+    columns.add(new QueryResultColumn("COLLATION_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("CHARACTER_SET_NAME", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("ID", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("IS_DEFAULT", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("IS_COMPILED", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("SORTLEN", "VARCHAR(255)"));
+    columns.add(new QueryResultColumn("PAD_ATTRIBUTE", "VARCHAR(255)"));
+    rows = new ArrayList<>();
+    row = new ArrayList<>();
+    row.add("utf8mb4_0900_ai_ci");
+    row.add("utf8mb4");
+    row.add("255");
+    row.add("Yes");
+    row.add("Yes");
+    row.add("0");
+    row.add("NO PAD");
+    rows.add(row);
+    mockResults.put(query, new Pair<>(columns, rows));
+
+    String pattern =
+        "(?i)SELECT .+ FROM .*information_schema.*\\..*routines.* WHERE .*routine_schema.* =.*";
     // SPECIFIC_NAME, ROUTINE_CATALOG, ROUTINE_SCHEMA, ROUTINE_NAME, ROUTINE_TYPE, DATA_TYPE,
     // CHARACTER_MAXIMUM_LENGTH, CHARACTER_OCTET_LENGTH, NUMERIC_PRECISION, NUMERIC_SCALE,
     // DATETIME_PRECISION, CHARACTER_SET_NAME, COLLATION_NAME, DTD_IDENTIFIER, ROUTINE_BODY,
